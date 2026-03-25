f1dfdc11f34bd6504cc7deba9d5552f46fe34447
xadrez44444 <alexandre.freitas.oliveira@escola.pr.gov.br>
2026-03-25 08:49:43 -0300

    media.js

---
+++ media.js
@@ -0,0 +1,11 @@
+// const nota1 = 10;
+// const nota2 = 6.5;
+// const nota3 = 8;
+// const nota4 = 7.5;
+
+//posiçoẽs       0  1  2  3 
+// const notas = [10];
+
+const media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+console.log(media);
